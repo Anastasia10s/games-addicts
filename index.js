b4508cebb92e74dbfe77be72e0cda74d44cee7f6
Anastasia Hayden <anastasiahayden@anastasias-mbp.lan>
2022-11-29 19:12:30 -0600

    feat: html-logic

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const readline = require("readline").createInterface({
 
 
 let board = [
-  ["X", "X", "-"],
+  ["X", "O", "O"],
 
   ["X", "-", "X"],
 
-  ["-", "X", "O"]
+  ["-", "O", "O"]
 ]
 
 const map = new Map()
@@ -17,24 +17,32 @@ map.set('1', { x: 0, y: 0 })
 map.set('2', { x: 0, y: 1 })
 map.set('3', { x: 0, y: 2 })
 map.set('4', { x: 1, y: 0 })
-map.set('5', { x: 0, y: 1 })
-map.set('6', { x: 0, y: 2 })
+map.set('5', { x: 1, y: 1 })
+map.set('6', { x: 1, y: 2 })
 map.set('7', { x: 2, y: 0 })
-map.set('8', { x: 0, y: 1 })
-map.set('9', { x: 0, y: 2 })
+map.set('8', { x: 2, y: 1 })
+map.set('9', { x: 2, y: 2 })
 
 let playing = true
 
-const checkBoard = (row) => {
+const checkBoard = () => {
   // We need to loop over this
-  if (row[0] !== '-') {
-    const rowStringTheSame = row.every(element => element === row[0])
-    if (rowStringTheSame) {
-      return { won: true, winner: row[0] }
+  for (let i = 0; i < board.length; i++) {
+    const row = board[i] /* 3 indexes of board, 1 for each row    */
+    if (row[0] !== '-'/* row[0] is the first item on the first array */) {
+      const rowStringTheSame = row.every(element => element === row[0] /* .every returns a boolean value  */)
+      if (rowStringTheSame) {
+        return { won: true, winner: row[0] }
+      }
     }
   }
   return { won: false, winner: '-' }
+
 }
+
+// let allRowsCheck = board.forEach((row) => {
+// row[i] === 'X'
+//} )
 
 const play = () => {
   readline.question("Where do you want to play? ", (spot) => {
@@ -47,14 +55,14 @@ const play = () => {
       // Deconstrucrt the object that is returned from map.get
       const { x, y } = map.get(spot)
       //access the row and column to set the value to x
-      board[x][y] = 'X'
+      board[x][y] = 'O'
     }
 
     // Show the board so I can see if it updated correctly
     console.log(`${board[0]} \n ${board[1]} \n ${board[2]}`);
 
 
-    if (checkBoard(board[0]).won === true) {
+    if (checkBoard().won === true) {
       console.log("Someone won")
     }
     //check for row 0 if x wins
