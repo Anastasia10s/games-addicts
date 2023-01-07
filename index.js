03ead22a611df2996a1b519d8fe52ec542110fc1
Anastasia Hayden <anastasiahayden@anastasias-mbp.lan>
2023-01-07 09:37:23 -0600

    feat:new logic

--- index.js
+++ index.js
@@ -1,76 +1,99 @@
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const PLAY_X = 'X';
+const PLAY_O = 'O';
+let turn = PLAY_X;
 
 
-let board = [
-  ["X", "O", "O"],
+let initialBoard = [
+  ["-", "-", "-"],
 
-  ["X", "-", "X"],
+  ["-", "-", "-"],
 
-  ["-", "O", "O"]
-]
+  ["-", "-", "-"]
+];
 
-const map = new Map()
-map.set('1', { x: 0, y: 0 })
-map.set('2', { x: 0, y: 1 })
-map.set('3', { x: 0, y: 2 })
-map.set('4', { x: 1, y: 0 })
-map.set('5', { x: 1, y: 1 })
-map.set('6', { x: 1, y: 2 })
-map.set('7', { x: 2, y: 0 })
-map.set('8', { x: 2, y: 1 })
-map.set('9', { x: 2, y: 2 })
+let board = JSON.parse(JSON.stringify(initialBoard));
+let count = 0;
 
-let playing = true
+const spotToBoard = new Map()
+spotToBoard.set('1', { x: 0, y: 0 })
+spotToBoard.set('2', { x: 0, y: 1 })
+spotToBoard.set('3', { x: 0, y: 2 })
+spotToBoard.set('4', { x: 1, y: 0 })
+spotToBoard.set('5', { x: 1, y: 1 })
+spotToBoard.set('6', { x: 1, y: 2 })
+spotToBoard.set('7', { x: 2, y: 0 })
+spotToBoard.set('8', { x: 2, y: 1 })
+spotToBoard.set('9', { x: 2, y: 2 })
 
-const checkBoard = () => {
-  // We need to loop over this
-  for (let i = 0; i < board.length; i++) {
-    const row = board[i] /* 3 indexes of board, 1 for each row    */
-    if (row[0] !== '-'/* row[0] is the first item on the first array */) {
-      const rowStringTheSame = row.every(element => element === row[0] /* .every returns a boolean value  */)
-      if (rowStringTheSame) {
-        return { won: true, winner: row[0] }
-      }
-    }
-  }
-  return { won: false, winner: '-' }
-
-}
-
-// let allRowsCheck = board.forEach((row) => {
-// row[i] === 'X'
-//} )
-
-const play = () => {
-  readline.question("Where do you want to play? ", (spot) => {
-    console.log("SPOT", spot)
-
-    // T
-    if (!map.get(spot)) {
-      console.log("Hey what are you trying to do??")
-    } else {
-      // Deconstrucrt the object that is returned from map.get
-      const { x, y } = map.get(spot)
-      //access the row and column to set the value to x
-      board[x][y] = 'O'
-    }
-
-    // Show the board so I can see if it updated correctly
-    console.log(`${board[0]} \n ${board[1]} \n ${board[2]}`);
-
-
-    if (checkBoard().won === true) {
-      console.log("Someone won")
-    }
-    //check for row 0 if x wins
-    //output the winner
-    //close the readline
-    readline.close()
-
-  });
+const updateBoard = (spot) => {
+  count++;
+  spot.innerHTML = turn;
+  spot.setAttribute("disabled", true);
+  const { x, y } = spotToBoard.get(spot.id);
+  board[x][y] = turn;
 };
 
-play()
+const checkForWinner = (id) => {
+  //check for the row they clicked on
+  if (board[x][0] === board[x][1] && board[x][0] === board[x][2]) {
+    return "won";
+
+    // check for the column they clicked on
+  } else if (board[0][y] === board[1][y] && board[0][y] === board[2][y]) {
+    return "won";
+  } else if (count === board[0].length * board.length) {
+    return "cat";
+  } else if (
+    board[0][0] !== "-" &&
+    board[0][0] === board[1][1] &&
+    board[0][0] === board[2][2]
+  ) {
+    return "won";
+  }
+
+  // check fwd slash diagonal
+  // check back slash diagonal
+  // else if() {}
+
+  return "continue";
+
+};
+
+// Func Switch player's turn
+const switchPlayers = () => {
+  turn = turn === PLAY_X ? PLAY_O : PLAY_X;
+};
+
+// Func Reset Game
+const resetGame = () => {
+  for (let i = 1; i <= board.length * board[0].length; i++) {
+    const spot = document.getElementById(i);
+    spot.innerHTML = `-`;
+    spot.removeAttribute("disabled");
+  }
+
+  turn = PLAY_X;
+  board = JSON.parse(JSON.stringify(initialBoard));
+};
+
+// Func Start Game?
+
+//Onclick Game Logic the onclick game handler connecting HTML and JS
+
+const onclickSpotGameLogic = (event) => {
+  const spot = event.target;
+  const id = spot.id;
+
+  updateBoard(spot);
+  const won = checkForWinner(id);
+
+  if (won === "won") {
+    document.getElementById("game_info").innerHTML = `${turn} won the game!!`;
+  } else if (won === "cat") {
+    document.getElementById("game_info").innerHTML = `Cat's game!!`;
+  } else {
+    switchPlayers();
+  }
+
+  // debugger;
+}
